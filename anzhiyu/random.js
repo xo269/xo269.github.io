55dfe110c9f8d3e3ad70ad3e4ac208b16f2654d8
xo269 <c2021@foxmail@.com>
2025-05-17 05:24:08 +0800

    Site updated: 2025-05-17 05:24:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/16/hello-world/"];function toRandomPost(){
+var posts=["2025/05/16/hello-world/","2025/05/17/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
